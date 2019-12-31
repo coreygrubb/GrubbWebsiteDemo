9acc98005f0d821979779b66532cc73d598e49d0
coreygrubb <coreygrubb15@gmail.com>
2019-12-31 16:18:59 -0500

    AWS API Integration
    Integrating a call to a AWS Gateway API that calls a lambda expression to query the github API and making CSS adjustments to projects category

---
+++ CoreyGrubbResume/Scripts/OnClicked.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+const UserAction = async () => {
+    const response = await fetch('https://eel4uwkil0.execute-api.us-east-2.amazonaws.com/PROD/my-resource/?followers=12');
+    const myJson = await response.json();
+
+    var githubResponse = JSON.parse(myJson.body);
+    $('#name').html(githubResponse.name);
+    $('#url').html(githubResponse.url);
+};
