a4648b614ce566b2dfbda659a8f5837c82e5e1de
coreygrubb <coreygrubb15@gmail.com>
2020-01-06 17:49:10 -0500

    Handling no github repo returned
    If no repo is returned from the fetch it will now display a message indicating that

--- CoreyGrubbResume/Scripts/OnClicked.js
+++ CoreyGrubbResume/Scripts/OnClicked.js
@@ -1,10 +1,20 @@
 ﻿"use strict";
 
 const UserAction = async () => {
-    const response = await fetch('https://eel4uwkil0.execute-api.us-east-2.amazonaws.com/PROD/my-resource/?followers=12');
+    var followers = document.getElementById('followers').value;
+    const response = await fetch('https://eel4uwkil0.execute-api.us-east-2.amazonaws.com/PROD/my-resource/?followers=' + followers);
     const myJson = await response.json();
+    
+    try{
+        var githubResponse = JSON.parse(myJson.body);
 
-    var githubResponse = JSON.parse(myJson.body);
-    $('#name').html(githubResponse.name);
-    $('#url').html(githubResponse.url);
+        $('#name').html(githubResponse.name);
+        $('#url').html(githubResponse.html_url);
+        $('#description').html(githubResponse.description);
+    }
+    catch(error){
+        $('#name').html("");
+        $('#url').html("");
+        $('#description').html("Unable to find a repository for the supplied number of followers");
+    }
 };
